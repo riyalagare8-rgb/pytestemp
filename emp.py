16fe67ef9b0aa8fb408af1a125cc19c9ed07bf24
riyalagare8-rgb <riyalagare8@gmail.com>
2025-12-02 09:58:58 +0530

    Refactor employee_details function and format output

--- emp.py
+++ emp.py
@@ -1,16 +1,16 @@
-def employee_details (name,emp_id,department,salary):
+import pytest
+def employee_details(name,emp_id,department,salary):
 
     result = (
-       f"Employee Name: {name}\n"
-       f"Employee ID: {emp_id}\n"
-       f"Department: {department}\n"
-       f"salary: {salary}"
- )
-
+       f"Employee Name:{name}\n"
+       f"Employee ID:{emp_id}\n"
+       f"Department:{department}\n"
+       f"Salary:{salary}"
+    )
     return result
-if __name__ == "___main___":
-    name = "Alice"
-    emp_id = "E1001"
-    departmant = "IT"
-    salary = 55000
-    print (employee_details (name,emp_id,department,salary))
+if __name__=="___main___":
+    name="Alice"
+    emp_id="E1001"
+    departmant="IT"
+    salary=55000
+    print(employee_details(name,emp_id,department,salary))
